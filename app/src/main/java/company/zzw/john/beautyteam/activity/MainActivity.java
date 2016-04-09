f732c7f75d3703547454861b8f665ced83e0aabf
john <1256618728@qq.com>
2016-04-09 11:09:01 +0800

    Second

--- app/src/main/java/company/zzw/john/beautyteam/activity/MainActivity.java
+++ app/src/main/java/company/zzw/john/beautyteam/activity/MainActivity.java
@@ -2,32 +2,69 @@ package company.zzw.john.beautyteam.activity;
 
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
-import java.util.zip.Inflater;
+import com.lidroid.xutils.view.annotation.ContentView;
 
 import company.zzw.john.beautyteam.R;
-import company.zzw.john.beautyteam.fragment.HomeFragment;
-import company.zzw.john.beautyteam.fragment.NewsFragment;
-import company.zzw.john.beautyteam.fragment.PersonalFragment;
-
+import company.zzw.john.beautyteam.fragment.NoticeFragment;
+import company.zzw.john.beautyteam.fragment.TeamFragment;
+import company.zzw.john.beautyteam.fragment.MainFragment;
+@ContentView(R.layout.activity_main)
 public class MainActivity extends FragmentActivity {
 
     //定义FragmentTabHost对象
     private FragmentTabHost mTabHost;
 
-    private Inflater layoutinflater;
+    private LayoutInflater layoutinflater;
     //定义数组来存放Fragment界面
-    private Class fragmentArray[] = {HomeFragment.class, NewsFragment.class, PersonalFragment.class};
+    private Class fragmentArray[] = {NoticeFragment.class, TeamFragment.class, MainFragment.class};
     //定义数组来存放选项的图片
-    private int mImagViewArray[] ={};
+    private int mImagViewArray[] ={R.drawable.selector_tab_home,R.drawable.selector_tab_news,
+            R.drawable.selector_tab_personal};
+    //定义选项卡的文字
+    private String mTextView[] ={"Notice","Team","Main"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        com.lidroid.xutils.ViewUtils.inject(this);
+        initView();
+    }
+
+    /**
+     * 初始化组件
+     */
+    private void initView() {
+        layoutinflater= LayoutInflater.from(this);
+        mTabHost= (FragmentTabHost) findViewById(R.id.tab_content);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realTabContent);
+        //得到fragment的个数
+        int count=fragmentArray.length;
+        for (int i=0;i<count;i++){
+            //为每一个Tab设置文字以及图片
+            TabHost.TabSpec tabSpec=mTabHost.newTabSpec(mTextView[i]).setIndicator(getTabImageView(i));
+            //将Tab按钮添加到Tab选项卡中
+            mTabHost.addTab(tabSpec,fragmentArray[i],null);
+        }
+        mTabHost.getTabWidget().setDividerDrawable(null);
+    }
+    /**
+     * 给Tab按钮设置图标和文字
+     */
+    private View getTabImageView(int index){
+        View view=layoutinflater.inflate(R.layout.tab_item,null);
+        ImageView iv_image= (ImageView) view.findViewById(R.id.iv_image);
+        iv_image.setImageResource(mImagViewArray[index]);
+        TextView tv_text= (TextView) view.findViewById(R.id.tv_text);
+        tv_text.setText(mTextView[index]);
+        return view;
     }
 }
