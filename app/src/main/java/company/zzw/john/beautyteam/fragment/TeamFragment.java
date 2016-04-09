8feeac4bde1b119e8ff7e697165e6bcf2e2fe4e1
john <1256618728@qq.com>
2016-04-09 11:08:40 +0800

    Second

--- app/src/main/java/company/zzw/john/beautyteam/fragment/TeamFragment.java
+++ app/src/main/java/company/zzw/john/beautyteam/fragment/TeamFragment.java
@@ -1,19 +1,22 @@
 package company.zzw.john.beautyteam.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lidroid.xutils.ViewUtils;
+
+import company.zzw.john.beautyteam.R;
+
 /**
  * Created by john on 2016/4/5.
  */
-public class HomeFragment extends BaseFragment {
-
-
+public class TeamFragment extends BaseFragment {
     @Override
     public View initViews(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        View view=inflater.inflate(R.layout.fragment_team,container,false);
+        ViewUtils.inject(this,view);
+        return view;
     }
 }
