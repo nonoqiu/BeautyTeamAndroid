76e5ecd257dae2a9add47edc659a0f8231289096
john <1256618728@qq.com>
2016-04-09 11:17:26 +0800

    Second

--- app/src/main/java/company/zzw/john/beautyteam/fragment/MainFragment.java
+++ app/src/main/java/company/zzw/john/beautyteam/fragment/MainFragment.java
@@ -1,17 +1,22 @@
 package company.zzw.john.beautyteam.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.lidroid.xutils.ViewUtils;
+
+import company.zzw.john.beautyteam.R;
+
 /**
  * Created by john on 2016/4/5.
  */
-public class PersonalFragment extends BaseFragment {
+public class MainFragment extends BaseFragment {
     @Override
     public View initViews(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null;
+        View view=inflater.inflate(R.layout.fragment_main, container, false);
+        ViewUtils.inject(this,view);
+        return view;
     }
 }
